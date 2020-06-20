9f210a9ac3fe4d34b7b449b2e367a79989182b3a
Micah <thoresonm1@gmail.com>
2020-06-20 11:31:34 -0400

    Continue to doubt that this is the best way, doing it anyways

---
+++ WPFGameApp1/Models/Items/Equipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFGameApp1.Models.Items
+{
+    class Equipment : Item
+    {
+        #region PROPERTIES
+        private int _armorHard;
+        private double _armorSoft;
+
+        public int ArmorHard
+        {
+            get { return _armorHard; }
+            set { _armorHard = value; }
+        }
+
+        public double ArmorSoft
+        {
+            get { return _armorSoft; }
+            set { _armorSoft = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ WPFGameApp1/Models/Items/EquipmentActive.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFGameApp1.Models.Items
+{
+    class EquipmentActive : Equipment
+    {
+		#region PROPERTIES
+		private int _castsPerTurn;
+		private double _focusMod;
+		private double _enchantProficiency;
+		private Spell _boundSpell;
+
+
+		public int CastsPerTurn
+		{
+			get { return _castsPerTurn; }
+			set { _castsPerTurn = value; }
+		}
+
+		public double FocusMod
+		{
+			get { return _focusMod; }
+			set { _focusMod = value; }
+		}
+
+		public double EnchantProficiency
+		{
+			get { return _enchantProficiency; }
+			set { _enchantProficiency = value; }
+		}
+
+		public Spell BoundSpell
+		{
+			get { return _boundSpell; }
+			set { _boundSpell = value; }
+		}
+
+		#endregion
+
+	}
+}
